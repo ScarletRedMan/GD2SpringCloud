1b3bc61ca04f7db396a5bbb311bbb26ec1127512
ScarletRedMan <terentev.andrey.2002@gmail.com>
2024-02-22 15:50:47 +0700

    Добавлена основа проекта для сервиса 'account-statistic'

---
+++ account-statistic/src/test/java/com/example/accountstatistic/AccountStatisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountstatistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountStatisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
